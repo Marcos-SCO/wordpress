664c68a9ffab580e6f5ad0a6e650039fa7046dfb
Marcos-SCO <marcos_sco@outlook.com>
2021-01-31 20:06:13 -0300

    footer menu

--- wordpress_theme_from_scratch/wp-content/themes/scratch_theme/functions.php
+++ wordpress_theme_from_scratch/wp-content/themes/scratch_theme/functions.php
@@ -42,5 +42,6 @@ register_nav_menus(
     [
         'top-menu' => 'Top Menu Location',
         'mobile-menu' => 'Mobile Menu Location',
+        'footer-menu' => 'Footer Menu Location',
     ]
 );
