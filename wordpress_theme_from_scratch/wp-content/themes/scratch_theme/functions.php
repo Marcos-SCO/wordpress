d9ffa1ea5155ad72c839c3846c5bef7d1fe9e0f5
Marcos-SCO <marcos_sco@outlook.com>
2021-01-31 17:17:54 -0300

    function to handle js

--- wordpress_theme_from_scratch/wp-content/themes/scratch_theme/functions.php
+++ wordpress_theme_from_scratch/wp-content/themes/scratch_theme/functions.php
@@ -1,16 +1,28 @@
 <?php
 
-$templateUri = get_template_directory_uri(); 
+$templateUri = get_template_directory_uri();
 
-$bootstrapCssDir = $templateUri . '/css/bootstrap.min.css';
+$css_bootstrap = $templateUri . '/css/bootstrap.min.css';
+$js_bootstrap = $templateUri . '/js/bootstrap.min.js';
 
 /**
  * Function to handle bootstrap css
  */
 function load_css()
 {
-    global $bootstrapCssDir;
-    wp_register_style('bootstrap', $bootstrapCssDir, [], false);
+    global $css_bootstrap;
+    wp_register_style('bootstrap', $css_bootstrap, [], false);
     wp_enqueue_style('bootstrap');
 }
-add_action('wp_enqueue_scripts','load_css');
+add_action('wp_enqueue_scripts', 'load_css');
+
+/**
+ * Function to handle js
+ */
+function load_js()
+{
+    global $js_bootstrap;
+    wp_register_script('bootstrap', $js_bootstrap, [], true);
+    wp_enqueue_script('bootstrap');
+}
+add_action('wp_enqueue_scripts', 'load_js');
