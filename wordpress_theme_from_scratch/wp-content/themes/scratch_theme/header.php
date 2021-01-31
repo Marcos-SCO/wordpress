74fb0236cf946d66bf03c3278db9b67c9f61d455
Marcos-SCO <marcos_sco@outlook.com>
2021-01-31 17:22:55 -0300

    the_title function

--- wordpress_theme_from_scratch/wp-content/themes/scratch_theme/header.php
+++ wordpress_theme_from_scratch/wp-content/themes/scratch_theme/header.php
@@ -4,7 +4,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title><?= $args['title'] ?? 'Olá mundo' ?></title>
+    <title><?= the_title(); ?></title>
 
     <?php wp_head() ?>
 
