cddb30645c9ed0689b47b2c9152881ea1089c4d5
Marcos-SCO <marcos_sco@outlook.com>
2021-01-31 18:18:59 -0300

    page

--- wordpress_theme_from_scratch/wp-content/themes/scratch_theme/page.php
+++ wordpress_theme_from_scratch/wp-content/themes/scratch_theme/page.php
@@ -1,1 +1,13 @@
-<?php
+<?php get_header('header.php', ['title' => 'Scratch Page']) ?>
+
+
+<div class="container">
+
+    <h1><?= the_title() ?></h1>
+
+    <?= get_template_part('includes/section', 'content') ?>
+
+</div>
+
+
+<?php get_footer() ?>
