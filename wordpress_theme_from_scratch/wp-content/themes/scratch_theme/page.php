9758c882db49bcf378ea6523fe593d024526be3f
Marcos-SCO <marcos_sco@outlook.com>
2021-01-31 19:22:39 -0300

    page-wrap

--- wordpress_theme_from_scratch/wp-content/themes/scratch_theme/page.php
+++ wordpress_theme_from_scratch/wp-content/themes/scratch_theme/page.php
@@ -1,13 +1,15 @@
 <?php get_header('header.php', ['title' => 'Scratch Page']) ?>
 
 
-<div class="container">
+<section class="page-wrap">
+    <div class="container">
 
-    <h1><?= the_title() ?></h1>
+        <h1><?= the_title() ?></h1>
 
-    <?= get_template_part('includes/section', 'content') ?>
+        <?= get_template_part('includes/section', 'content') ?>
 
-</div>
+    </div>
+</section>
 
 
 <?php get_footer() ?>
--- wordpress_theme_from_scratch/wp-content/themes/scratch_theme/template/contact.php
+++ wordpress_theme_from_scratch/wp-content/themes/scratch_theme/template/contact.php
@@ -7,20 +7,22 @@ Template Name: Contato template
 <?php get_header('header.php', ['title' => 'Scratch Page']) ?>
 
 
-<div class="container">
-
-    <h1><?= the_title() ?></h1>
-
-    <div class="row">
-        <div class="col lg-6">
-            Formulário de contato
+<section class="page-wrap">
+    <div class="container">
+    
+        <h1><?= the_title() ?></h1>
+    
+        <div class="row">
+            <div class="col lg-6">
+                Formulário de contato
+            </div>
+            <div class="col lg-6">
+                <?= get_template_part('includes/section', 'content') ?>
+            </div>
         </div>
-        <div class="col lg-6">
-            <?= get_template_part('includes/section', 'content') ?>
-        </div>
+    
     </div>
-
-</div>
+</section >
 
 
 <?php get_footer() ?>
