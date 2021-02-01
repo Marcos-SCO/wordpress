bf80b146dacadc3fcb0c3c8509431a68f14a5241
Marcos-SCO <marcos_sco@outlook.com>
2021-02-01 11:39:09 -0300

    displaying sidebars widgets

--- wordpress_theme_from_scratch/wp-content/themes/scratch_theme/page.php
+++ wordpress_theme_from_scratch/wp-content/themes/scratch_theme/page.php
@@ -1,17 +1,28 @@
-<?php get_header('header.php', ['title' => 'Scratch Page']) ?>
+<?php get_header() ?>
 
 
 <section class="page-wrap">
-    <div class="container">
+    <div class="container justify-content-center">
 
-        <h1><?= the_title() ?></h1>
+        <section class="row">
 
-        <?php if (has_post_thumbnail()) : ?>
-            <img src="<?php the_post_thumbnail_url('blog-small') ?>" alt="<?php the_title() ?>" class="img-fluid mb-3 img-thumbnail me-4">
-        <?php endif; ?>
+            <?php if (is_active_sidebar('page-sidebar')) : ?>
+                <div class="col-lg-3">
+                    <?php dynamic_sidebar('page-sidebar'); ?>
+                </div>
+            <?php endif; ?>
 
-        <?= get_template_part('includes/section', 'content') ?>
+            <div class="col-lg-9">
+                <h1><?= the_title() ?></h1>
 
+                <?php if (has_post_thumbnail()) : ?>
+                    <img src="<?php the_post_thumbnail_url('blog-small') ?>" alt="<?php the_title() ?>" class="img-fluid mb-3 img-thumbnail me-4">
+                <?php endif; ?>
+
+                <?= get_template_part('includes/section', 'content') ?>
+            </div>
+
+        </section>
     </div>
 </section>
 
