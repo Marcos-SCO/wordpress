8545c4983836200a3e9f9ea74fbb32284b30effd
Marcos-SCO <marcos_sco@outlook.com>
2021-01-31 23:15:32 -0300

    page has post thumbnail

--- wordpress_theme_from_scratch/wp-content/themes/scratch_theme/page.php
+++ wordpress_theme_from_scratch/wp-content/themes/scratch_theme/page.php
@@ -6,6 +6,10 @@
 
         <h1><?= the_title() ?></h1>
 
+        <?php if (has_post_thumbnail()) : ?>
+            <img src="<?php the_post_thumbnail_url('blog-small') ?>" alt="<?php the_title() ?>" class="img-fluid mb-3 img-thumbnail me-4">
+        <?php endif; ?>
+
         <?= get_template_part('includes/section', 'content') ?>
 
     </div>
