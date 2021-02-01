0905f3301aa9b9c10217378600abf0fa32a5ac81
Marcos-SCO <marcos_sco@outlook.com>
2021-02-01 11:55:54 -0300

    blog grid

--- wordpress_theme_from_scratch/wp-content/themes/scratch_theme/includes/section-archive.php
+++ wordpress_theme_from_scratch/wp-content/themes/scratch_theme/includes/section-archive.php
@@ -2,7 +2,7 @@
 if (have_posts()) : while (have_posts()) : the_post();
 ?>
         <div class="card mb-3">
-            <div class="card-body d-flex justify-content-center align-items-center">
+            <div class="card-body d-flex flex-md-row flex-column justify-content-center align-items-center">
                 <?php if (has_post_thumbnail()) : ?>
                     <img src="<?php the_post_thumbnail_url('blog-small') ?>" alt="<?php the_title() ?>" class="img-fluid mb-3 img-thumbnail me-4">
                 <?php endif; ?>
