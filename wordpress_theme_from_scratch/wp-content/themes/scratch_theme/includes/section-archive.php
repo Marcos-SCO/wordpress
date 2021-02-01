f3723660bf46a8fa1c42c75bd324e190523a5776
Marcos-SCO <marcos_sco@outlook.com>
2021-01-31 23:08:48 -0300

    img thumbnails

--- wordpress_theme_from_scratch/wp-content/themes/scratch_theme/includes/section-archive.php
+++ wordpress_theme_from_scratch/wp-content/themes/scratch_theme/includes/section-archive.php
@@ -2,11 +2,17 @@
 if (have_posts()) : while (have_posts()) : the_post();
 ?>
         <div class="card mb-3">
-            <div class="card-body">
-                <h3><?= the_title() ?></h3>
-                <?//= the_content(); ?>
-                <?= the_excerpt(); ?>
-                <a href="<?= the_permalink() ?>" class="btn btn-success">Ler mais</a>
+            <div class="card-body d-flex justify-content-center align-items-center">
+                <?php if (has_post_thumbnail()) : ?>
+                    <img src="<?php the_post_thumbnail_url('blog-small') ?>" alt="<?php the_title() ?>" class="img-fluid mb-3 img-thumbnail me-4">
+                <?php endif; ?>
+
+                <div class="blog-content">
+                    <h3><?= the_title() ?></h3>
+                    <?//= the_content(); ?>
+                    <?= the_excerpt(); ?>
+                    <a href="<?= the_permalink() ?>" class="btn btn-success">Ler mais</a>
+                </div>
             </div>
         </div>
 <?php endwhile;
