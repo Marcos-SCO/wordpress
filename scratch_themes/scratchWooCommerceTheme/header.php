7616a3af2aae9b6d137dacf915c3450a4ccecbd7
Marcos-SCO <marcos_sco@outlook.com>
2021-04-26 16:49:34 -0300

    wp body functions

--- scratch_themes/scratchWooCommerceTheme/header.php
+++ scratch_themes/scratchWooCommerceTheme/header.php
@@ -5,7 +5,8 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title><?=the_title()?></title>
+    <title><?= the_title() ?></title>
+    <?php wp_head(); ?>
 </head>
 
-<body>
+<body <?php body_class('test'); ?>>
