728ccbc7adf33263217dfde9d3594cd7b1250ae3
Marcos-SCO <marcos_sco@outlook.com>
2021-04-26 17:28:10 -0300

    css and script load functions

--- scratch_themes/scratchWooCommerceTheme/functions.php
+++ scratch_themes/scratchWooCommerceTheme/functions.php
@@ -1,6 +1,23 @@
 <?php
+
 function admin_bar()
 {
     if (is_user_logged_in())  add_filter('show_admin_bar', '__return_true', 1000);
 }
 add_action('init', 'admin_bar');
+
+function load_stylesheets()
+{
+    // Name handler / Source / Dependencies / Version / Media
+    wp_register_style('stylesheet', get_template_directory_uri() . '/assets/css/style.css', '', 1, 'all');
+    wp_enqueue_style('stylesheet');
+}
+add_action('wp_enqueue_scripts', 'load_stylesheets');
+
+
+function load_scripts()
+{
+    wp_register_script('custom', get_template_directory_uri() . '/assets/js/app.js', 'jquery', 1, true);
+    wp_enqueue_script('custom');
+}
+add_action('wp_enqueue_scripts', 'load_scripts');
